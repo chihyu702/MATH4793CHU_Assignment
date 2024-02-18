2fc0a0ad09e3a470653a24f9bd4af3c48e4f845c
chihyu <chih.yu.chu-1@ou.edu>
2024-02-17 21:30:46 -0600

    ASS1_1

---
+++ R/T1_5.R
@@ -0,0 +1,17 @@
+#' T1-5 data
+#'
+#' This is a data about air-pollution. This contains 42 measurements with 7 variables
+#'
+#' @format A data frame with 42 rows and 7 columns
+#' \describe{
+#'   \item{V1}{Wind}
+#'   \item{V2}{Solar radiation}
+#'   \item{V3}{CO}
+#'   \item{V4}{NO}
+#'   \item{V5}{NO2}
+#'   \item{V6}{O3}
+#'   \item{V7}{HC}
+#'   ...
+#' }
+#' @source text book
+"T1_5"
